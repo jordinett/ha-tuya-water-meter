934ff2596a81b42407d25772801d4e8353f77f2b
jordinett <guineu2@hotmail.com>
2026-07-12 20:18:15 +0200

    Create __init__.py

---
+++ custom_components/tuya_water_meter/__init__.py
@@ -0,0 +1,41 @@
+"""The Tuya Water Meter integration."""
+
+from __future__ import annotations
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+PLATFORMS: list[str] = []
+
+
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    """Set up the Tuya Water Meter integration."""
+    return True
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+    """Set up Tuya Water Meter from a config entry."""
+
+    await hass.config_entries.async_forward_entry_setups(
+        entry,
+        PLATFORMS,
+    )
+
+    return True
+
+
+async def async_unload_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+    """Unload a config entry."""
+
+    return await hass.config_entries.async_unload_platforms(
+        entry,
+        PLATFORMS,
+    )
