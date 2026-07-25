d65c8d04a533b120b8b3c5f1e7bb185c39ac30b9
jordinett <guineu2@hotmail.com>
2026-07-25 16:41:05 +0200

    Update const.py

--- custom_components/tuya_water_meter/const.py
+++ custom_components/tuya_water_meter/const.py
@@ -1,12 +1,10 @@
 """Constants for the Tuya Water Meter integration."""
 
 DOMAIN = "tuya_water_meter"
-
 NAME = "Tuya Water Meter"
-
 VERSION = "0.3.0"
 
 CONF_CLIENT_ID = "client_id"
 CONF_CLIENT_SECRET = "client_secret"
-
-DEFAULT_API_REGION = "eu"
+CONF_UID = "uid"
+CONF_REGION = "region"
