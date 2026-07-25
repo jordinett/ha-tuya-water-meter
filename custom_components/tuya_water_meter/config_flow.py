3347b58d8921d4ed3f5740fabc68e6bde8c6897f
jordinett <guineu2@hotmail.com>
2026-07-25 16:22:51 +0200

    Update config_flow.py

--- custom_components/tuya_water_meter/config_flow.py
+++ custom_components/tuya_water_meter/config_flow.py
@@ -2,13 +2,10 @@
 
 from __future__ import annotations
 
-import aiohttp
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from .api import TuyaCloudApi, TuyaCloudApiError
 from .const import CONF_CLIENT_ID, CONF_CLIENT_SECRET, DOMAIN
 
 
@@ -26,99 +23,22 @@ class TuyaWaterMeterConfigFlow(
     ):
         """Handle the initial setup step."""
 
-        errors = {}
-
-        if user_input is None:
-            self.hass.logger.warning(
-                "TUYA WATER METER DEBUG: Config flow started."
-            )
+        self.hass.logger.warning(
+            "TUYA WATER METER TEST: Config flow executed."
+        )
 
         if user_input is not None:
             self.hass.logger.warning(
-                "TUYA WATER METER DEBUG: User submitted credentials."
+                "TUYA WATER METER TEST: Submit button pressed."
             )
 
-            try:
-                self.hass.logger.warning(
-                    "TUYA WATER METER DEBUG: Creating API client."
-                )
-
-                session = async_get_clientsession(self.hass)
-
-                api = TuyaCloudApi(
-                    session=session,
-                    client_id=user_input[CONF_CLIENT_ID],
-                    client_secret=user_input[CONF_CLIENT_SECRET],
-                )
-
-                self.hass.logger.warning(
-                    "TUYA WATER METER DEBUG: API client created."
-                )
-
-                self.hass.logger.warning(
-                    "TUYA WATER METER DEBUG: Requesting token."
-                )
-
-                token_data = await api.async_get_token()
-
-                self.hass.logger.warning(
-                    "TUYA WATER METER DEBUG: Token received."
-                )
-
-                uid = token_data.get("uid")
-
-                self.hass.logger.warning(
-                    "TUYA WATER METER DEBUG: UID received: %s",
-                    uid,
-                )
-
-                if not uid:
-                    raise TuyaCloudApiError(
-                        "Tuya Cloud did not return a user ID."
-                    )
-
-                self.hass.logger.warning(
-                    "TUYA WATER METER DEBUG: Requesting device list."
-                )
-
-                devices = await api.async_get_user_devices(uid)
-
-                self.hass.logger.warning(
-                    "TUYA WATER METER DEBUG: Device list received. "
-                    "Found %d devices.",
-                    len(devices),
-                )
-
-                for device in devices:
-                    self.hass.logger.warning(
-                        "TUYA WATER METER DEBUG: Device found: "
-                        "name=%s, id=%s, category=%s",
-                        device.get("name"),
-                        device.get("id"),
-                        device.get("category"),
-                    )
-
-            except Exception as err:
-                self.hass.logger.exception(
-                    "TUYA WATER METER DEBUG: Exception occurred: %s",
-                    err,
-                )
-
-                errors["base"] = "cannot_connect"
-
-            else:
-                self.hass.logger.warning(
-                    "TUYA WATER METER DEBUG: Creating config entry."
-                )
-
-                return self.async_create_entry(
-                    title="Tuya Water Meter",
-                    data={
-                        CONF_CLIENT_ID: user_input[CONF_CLIENT_ID],
-                        CONF_CLIENT_SECRET: user_input[CONF_CLIENT_SECRET],
-                        "uid": uid,
-                    },
-                )
+            return self.async_create_entry(
+                title="Tuya Water Meter",
+                data={
+                    CONF_CLIENT_ID: user_input[CONF_CLIENT_ID],
+                    CONF_CLIENT_SECRET: user_input[CONF_CLIENT_SECRET],
+                },
+            )
 
         data_schema = vol.Schema(
             {
@@ -130,5 +50,4 @@ class TuyaWaterMeterConfigFlow(
         return self.async_show_form(
             step_id="user",
             data_schema=data_schema,
-            errors=errors,
         )
